24f3cc6410be91348f4dc857670ba0a552bb2e37
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-14 17:15:04 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilmProjectRails::Application.config.secret_token = '6d31687415c8ed00d862b5d587eaba2855e8f6be844fbadd7119677802318c24ef6964621a0f5c9f5001e42a84378747ad1291d94ae66fa46a107bd50025a63b'
