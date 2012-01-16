ec782afdc1e3b33fa4f81807dceab9a4bc892b6f
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-15 22:01:08 -0600

    added url /signup and added link from homepage to /signup also added template for users CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 FilmProjectRails::Application.routes.draw do
+  resources :users
+
   get "home/index"
 
 FilmProjectRails::Application.routes.draw do
    
+  resources :users
+  match '/signup',    :to => 'users#new'
   #...
   #  # You can have the root of your site routed with "root"
   #    # just remember to delete public/index.html.
