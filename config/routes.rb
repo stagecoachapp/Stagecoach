23ba0358b3b6a800fdbaf361f972cfca246b5233
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-16 15:41:44 -0600

    made nav bar highlight current page. also changed nave bar to logical names and added an about html stub

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ FilmProjectRails::Application.routes.draw do
    
   resources :users
   match '/signup',    :to => 'users#new'
+  match '/about', :to => 'home#about'
   #...
   #  # You can have the root of your site routed with "root"
   #    # just remember to delete public/index.html.
