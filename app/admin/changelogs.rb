cb195eee3edfc7ece206c299ac026c1012a57dfa
Gabriel Peal <gpeal@u.northwestern.edu>
2012-01-17 13:25:27 -0600

    added created_at field for changelog admin

--- app/admin/changelogs.rb
+++ app/admin/changelogs.rb
@@ -1,4 +1,12 @@
 ActiveAdmin.register Changelog do
-  
-	
+
+	form do |f|
+		f.inputs 'Details' do
+		  f.input :created_at
+		  f.input :developer
+		  f.input :description
+		end
+		f.buttons
+	end
+
 end
