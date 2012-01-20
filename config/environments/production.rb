ba368337001089e115c6c8ccd2d79a617d0b4b92
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-19 21:06:15 -0600

    added search.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 FilmProjectRails::Application.configure do
   #add active admin
   config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w( search.js )
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
