d91919e3546d1a7d55f6436f8d313e40f52b4f49
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-19 19:47:17 -0600

    second try on static assets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,6 +23,7 @@ FilmProjectRails::Application.configure do
 
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
+   config.assets.precompile += ['phone.css', 'application.css']
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
