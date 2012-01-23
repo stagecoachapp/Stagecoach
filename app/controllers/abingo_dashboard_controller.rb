a45319639b2d8b14162ab3c230ba5bd30faa9984
Jonathan Friedman <friedmanj98@gmail.com>
2012-01-22 19:12:28 -0600

    removed abingo!

--- app/controllers/abingo_dashboard_controller.rb
+++
@@ -1,7 +0,0 @@
-class AbingoDashboardController < ApplicationController
-  #Declare any before_filters or similar which you need to use your authentication
-  ##for this controller.
-  
-   include Abingo::Controller::Dashboard
-  # end
-end
