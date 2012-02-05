b62a5ce5d98b3adb2c0cc86acb6eac4ebcaee01b
Jonathan Friedman <friedmanj98@gmail.com>
2012-02-05 16:47:56 -0600

    active admin works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FilmProjectRails::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :users
 
   resources :user_roles
@@ -8,6 +12,10 @@ FilmProjectRails::Application.routes.draw do
 
 FilmProjectRails::Application.routes.draw do
    
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   resources :users
 
   resources :user_roles
