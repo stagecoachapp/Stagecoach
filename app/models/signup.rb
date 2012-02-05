243d4a4520581c01cc57aced5ea6aede83a27ebf
Gabriel Peal <gpeal@u.northwestern.edu>
2012-02-04 18:19:44 -0600

    created user model and added it to ActiveAdmin

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,2 +1,6 @@
 class Signup < ActiveRecord::Base
+  #include ActiveModel::Validations
+  validates :name, :presence => true, :length => { :minimum => 1 }
+  validates :email, :presence => true
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 end
--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  #include ActiveModel::Validations
-  validates :name, :presence => true, :length => { :minimum => 1 }
-  validates :email, :presence => true
-  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-end
