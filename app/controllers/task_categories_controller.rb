e520ce139c2df172db104bab773935324acd2b95
gpeal <gpeal@u.northwestern.edu>
2012-02-10 22:25:01 -0600

    task_categories is now dynamic

--- app/controllers/task_categories_controller.rb
+++ app/controllers/task_categories_controller.rb
@@ -1,2 +1,29 @@
-class TaskCategoriesController < InheritedResources::Base
+class TaskCategoriesController < ApplicationController
+
+  def index
+    @task_categories = TaskCategory.all
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def new
+    @task_category = TaskCategory.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @task_category = TaskCategory.new(params[:task_category])
+
+    if @task_category.save
+      respond_to do |format|
+        format.html { redirect_to task_categories_url}
+      end
+    end
+  end
+
 end
