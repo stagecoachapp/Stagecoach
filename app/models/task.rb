b6e6c2234c81a4c36f161ee693413f4fd91f8868
Jonathan Friedman <friedmanj98@gmail.com>
2012-02-12 14:24:43 -0600

    added some validations to the models (this is going to piss some people off) Also REMEMBER THAT YOU MUST SPECIFY A PROJECT WHEN YOU CREATE USERS etc
    Also moved some css/js files that were rougue

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,10 @@ class Task < ActiveRecord::Base
 	has_and_belongs_to_many :task_category
 	has_many :reminders
 	belongs_to :project
+	validates :name, :presence => true
+	validates :startdate, :presence => true
+	validates :enddate, :presence => true
+	validates_numericality_of :priority
+	validates :status, :presence => true
+	validates_numericality_of :project_id
 end
