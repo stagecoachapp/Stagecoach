e391bd494035c6c6470879bf884e446f5addddef
Ryan McAfee <mcafeeryan92@gmail.com>
2012-02-21 09:58:35 -0600

    fixing merge conflict

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-FilmProjectRails::Application.config.session_store :cookie_store, :key => '_film-project-rails_session'
+#FilmProjectRails::Application.config.session_store :cookie_store, :key => '_film-project-rails_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# FilmProjectRails::Application.config.session_store :active_record_store
+FilmProjectRails::Application.config.session_store :active_record_store
