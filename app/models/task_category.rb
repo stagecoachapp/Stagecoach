f57ec03d000b398c33b6253b22dfe03501de2be7
gpeal <gpeal@u.northwestern.edu>
2012-02-20 18:57:30 -0600

    Fixed the ability to assign task categories to tasks Added the ability to create a task category

--- app/models/task_category.rb
+++ app/models/task_category.rb
@@ -2,5 +2,5 @@ class TaskCategory < ActiveRecord::Base
 	has_and_belongs_to_many :tasks
 	belongs_to :project
 	validates :name, :presence => true
-	validates_numericality_of :project_id
+	#validates_numericality_of :project_id
 end
