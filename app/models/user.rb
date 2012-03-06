78aacedb996b54aef57bca64877b166f7f4d6c50
gpeal <gpeal@u.northwestern.edu>
2012-03-06 02:00:12 -0600

    Removed smartphone option from new user, edit, and show profile Removed Team from profile (user roles/teams aren't fully thought out for mvp)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ActiveRecord::Base
 	has_and_belongs_to_many :projects
  	has_one :authorization
 
+ 	after_initialize :default_values
+
   def self.create_from_hash!(auth)
     create(:name => auth['info']['name'], :email => auth['info']['email'])
   end
+
+  private
+  	def default_values
+  		self.smartphone ||= 1
+  	end
 end
