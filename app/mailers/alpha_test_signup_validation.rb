d5d0e398b680ffc25c066bb5c4de7e9f1eb959d6
Jonathan Friedman <friedmanj98@gmail.com>
2012-03-31 14:43:11 -0500

    Feature: First Stab at Sending Emails

---
+++ app/mailers/alpha_test_signup_validation.rb
@@ -0,0 +1,11 @@
+class AlphaTestSignupValidation < ActionMailer::Base
+  default :from => "notifications@projectstagecoach.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = "http://projectstagecoach.com"
+    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+  end
+end
+
+end
