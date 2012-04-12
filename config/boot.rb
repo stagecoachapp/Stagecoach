b47d3cd574569f52d6d6d0bef844b454541eadf4
gpeal <gpeal@u.northwestern.edu>
2012-04-12 17:06:13 -0500

    Change: moved ENV variables from boot.rb to .env NOTE: use 'foreman start' to run the server

--- config/boot.rb
+++ config/boot.rb
@@ -3,11 +3,4 @@ require 'rubygems'
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-ENV['AWS_ACCESS_KEY_ID'] = 'AKIAIB2PQYR2LSGIG4BA'
-ENV['AWS_SECRET_ACCESS_KEY'] = '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'
-ENV['FOG_DIRECTORY'] = 'stagecoachlocal-assets'
-ENV['FOG_PROVIDER'] = 'AWS'
-ENV['GMAIL_PASSWORD'] = 'northwestern2013'
-ENV['GMAIL_USERNAME'] = 'projectstagecoach@gmail.com'
-
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
