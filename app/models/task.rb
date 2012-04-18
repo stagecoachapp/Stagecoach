665a736b956ef2a9ce95c2dfbc7b3a2e0299e60c
gpeal <gpeal@u.northwestern.edu>
2012-04-18 01:33:10 -0500

    Bug Fix: Tasks no longer require a start or end date Bug Fix: fixed a bug related to viewing all tasks by project/user not being filtered correctly

--- app/models/task.rb
+++ app/models/task.rb
@@ -17,4 +17,20 @@ class Task < ActiveRecord::Base
 		self.name
 	end
 
+	def formatted_startdate
+		if self.startdate.nil?
+			return "No Start Date"
+		else
+			return self.startdate.strftime("%a %b %d %I:%M%p")
+		end
+	end
+
+	def formatted_enddate
+		if self.enddate.nil?
+			return "No End Date"
+		else
+			return self.enddate.strftime("%a %b %d %I:%M%p")
+		end
+	end
+
 end
