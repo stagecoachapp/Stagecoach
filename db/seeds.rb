c0302671d4f61dcc7c25b35cbb94570eecb5169c
gpeal <gpeal@u.northwestern.edu>
2012-04-21 21:40:58 -0500

    Change: removed seeds (will be readded to follow Kashif's template)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-
-NotificationType.create(:name => "Default")
-NotificationType.create(:name => "NewTask")
-TaskStatus.create(:name => "Pending")
-TaskStatus.create(:name => "Late")
-TaskStatus.create(:name => "Complete")
-TaskStatus.create(:name => "LateComplete")
-TaskPriority.create(:name => "Low")
-TaskPriority.create(:name => "High")
