031677de157c0fbc54cfeb61f24577f50bd9c3c8
Kalail <kashif610@gmail.com>
2012-04-22 21:20:04 -0500

    Change: Hash project passwords

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
+require 'bcrypt'
+
 class Project < ActiveRecord::Base
-	attr_accessible :name, :created_at, :updated_at, :password, :owner_id, :owner, :administrator, :administrators, :administrator_ids
+	include BCrypt
+
+	my_password = BCrypt::Password.create("my password")
+
+	attr_accessible :name, :created_at, :updated_at, :password_hash, :owner_id, :owner, :administrator, :administrators, :administrator_ids
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :administrators, :class_name => "User", :join_table => "administrators"
 	belongs_to :owner, :class_name => "User"
@@ -10,6 +16,16 @@ class Project < ActiveRecord::Base
 
 	validates :name, :presence => true
 	validates :owner, :presence => true
+
+	# Next 2 functions provide a layer of abstraction between password and password hash
+	def password
+      @password ||= Password.new(self.password_hash)
+    end
+
+    def password=(new_password)
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    end
 
 	def capitalized_name
 		name.split(' ').map {|w| w.capitalize }.join(' ')
