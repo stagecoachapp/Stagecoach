954ed6c330c322022150dd146ba61168c79ce3e2
gpeal <gpeal@u.northwestern.edu>
2012-04-25 11:06:27 -0500

    Feature: heroku procfile includes worker (untested) Bug Fix: signup email works

--- app/mailers/alpha_test_signup_validation.rb
+++ app/mailers/alpha_test_signup_validation.rb
@@ -1,10 +1,10 @@
 class AlphaTestSignupValidation < AsyncMailer
 	default :from => "#{ENV['GMAIL_USERNAME']}"
 
-	def welcome_email(user_id)
-		@user = User.find_by_id(user_id)
+	def welcome_email(signup_id)
+		@signup = Signup.find_by_id(signup_id)
 		@url  = "http://projectstagecoach.com"
-		mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+		mail(:to => @signup.email, :subject => "Welcome to My Awesome Site")
 	end
 end
 
