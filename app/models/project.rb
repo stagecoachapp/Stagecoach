e62f7c87e11a35e9d6a782fb6a2bf393188a298b
Jonathan Friedman <friedmanj98@gmail.com>
2012-06-02 21:19:49 -0500

    Feature: cleaned up random shit around the house

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,39 +3,39 @@ require 'bcrypt'
 class Project < ActiveRecord::Base
 	include BCrypt
 
-	attr_accessible :name, :created_at, :updated_at, :password_hash, :owner_id, :owner, :administrator, :administrators, :administrator_ids
-	has_and_belongs_to_many :users
-	has_and_belongs_to_many :administrators, :class_name => "User", :join_table => "administrators"
-	belongs_to :owner, :class_name => "User"
-	has_many :tasks
-	has_many :task_categories
-	has_many :user_roles
-	has_many :assets, :as => :asset_object, :dependent => :destroy
+    attr_accessible :name, :created_at, :updated_at, :password_hash, :owner_id, :owner, :administrator, :administrators, :administrator_ids
+    has_and_belongs_to_many :users
+    has_and_belongs_to_many :administrators, :class_name => "User", :join_table => "administrators"
+    belongs_to :owner, :class_name => "User"
+    has_many :tasks
+    has_many :task_categories
+    has_many :user_roles
+    has_many :assets, :as => :asset_object, :dependent => :destroy
 
-	validates :name, :presence => true
-	validates :owner, :presence => true
+    validates :name, :presence => true
+    validates :owner, :presence => true
 
 	# Next 2 functions provide a layer of abstraction between password and password hash
 	def password
       password = Password.new(self.password_hash)
-    end
+  end
 
-    def password=(new_password)
+  def password=(new_password)
       password = Password.create(new_password)
       self.password_hash = password
-    end
+  end
 
-	def capitalized_name
-		name.split(' ').map {|w| w.capitalize }.join(' ')
-	end
+  def capitalized_name
+      name.split(' ').map {|w| w.capitalize }.join(' ')
+  end
 
-	def owner_exists?
-		owner = User.find_by_id(self.owner_id)
-		!owner.nil?
-	end
+  def owner_exists?
+      owner = User.find_by_id(self.owner_id)
+      !owner.nil?
+  end
 
-	def to_s
-		self.capitalized_name
-	end
+  def to_s
+      self.capitalized_name
+  end
 
 end
